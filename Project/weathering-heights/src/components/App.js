05501e620f3272be40d0ee9b2e7fa383dedfe9c8
Lia Gaetano <lgaetano19@gmail.com>
2022-02-01 20:52:08 -0800

    Brings DB retreival back to App.js

--- Project/weathering-heights/src/components/App.js
+++ Project/weathering-heights/src/components/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useState, useEffect, useContext } from 'react';
+import { ref, onValue } from "firebase/database";
+import { db } from './../firebase.js';
 import { Container } from 'react-bootstrap'
 import { AuthProvider } from '../contexts/AuthContext';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -11,24 +13,44 @@ import ForgotPassword from './ForgotPassword';
 import Homepage from './Homepage';
 import MyProfile from './MyProfile';
 
+
 function App() {
   const [peakList, setPeakList] = useState([]);
   const [status, setStatus] = useState(true);
 
-  // // // Use this to view data
-  // console.log(getBulgerListData())
-
+  // GET peak data from FBDB
   useEffect(() => {
-    const peaksData = getBulgerListData();
-    
-    setPeakList(peaksData);
-    setStatus(false);
-  }, []);
-  
+    const bulgerListArr = [];
 
-  useEffect(() => {
-    console.log(peakList);
-  }, [peakList]);
+    const peaks = ref(db, 'peaks/');
+    onValue(peaks, (snapshot) => {
+        const data = snapshot.val();
+        for (let i = 0; i < data.length; i++) {
+            if (data[i]) { 
+                bulgerListArr.push({
+                    key: i,
+                    chance_precip: data[i].chance_precip,
+                    coordinates: data[i].coordinates,
+                    elevation: data[i].elevation,
+                    indigenous_name: data[i].indigenous_name,
+                    link: data[i].link,
+                    name: data[i].name,
+                    range: data[i].range,
+                    rank: data[i].rank,
+                    temp: data[i].temp,
+                    wind_speed: data[i].wind_speed,
+                });
+                };
+        };
+        setPeakList(bulgerListArr);
+      });
+    }, []);
+
+    // console.log(peakList)
+
+  // useEffect(() => {
+  //   console.log(peakList);
+  // }, [peakList]);
 
   return (
       <Container className="d-flex align-items-center" style={{ minHeight: "100vh" }}>
