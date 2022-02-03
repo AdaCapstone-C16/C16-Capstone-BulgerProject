c6867f95b575d6b6779faa2413789f3c7b0ac4a8
Roslyn Melookaran <rmelookaran@gmail.com>
2022-02-03 09:42:21 -0800

    trips are now getting added to the db

--- Project/weathering-heights/src/components/AddTrip.js
+++ Project/weathering-heights/src/components/AddTrip.js
@@ -2,46 +2,17 @@ import React from 'react';
 import { useState } from 'react';
 import '../components/stylesheets/AddSummit.css'
 import {db} from '../firebase'
-import { ref, onValue, set } from 'firebase/database';
-import Select from 'react-select'
-// import { get, ref, set, child } from "firebase/database";
+import { ref, set } from 'firebase/database';
 import { useAuth } from '../contexts/AuthContext'
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css'
 
 const AddTrip = (props) => {
-    // This section pulls peak info from the db
-    // const peaks = ref(db, 'peaks/');
-    // const peakNames = [];
-    // const { currentUser } = useAuth()
-    // const [summit, setSummit] = useState(['',''])
 
-    // onValue(peaks, (snapshot) => {
-    //     const data = snapshot.val();
-    //     let count = 0
-    //     for (let peak of data){
-    //         if (peak && peak.indigenous_name) {
-    //             peakNames.push({value:count,label:`${peak.indigenous_name} [${peak.name}]`})
-    //         } else if (peak) {
-    //             peakNames.push({value:count, label:peak.name})
-    //         };
-    //         count++;
-    //     }
-    // })
-    
-    
-    // const handleSummitAdd = (event) => {
-    //     setSummit([event.value, event.label])
-    // }
-    
-    // const handleAddDB = () => {
-    //     console.log(summit)
-    //     set(ref(db, `users/${currentUser.uid}/summits/${summit[0]}`), {name:summit[1]})
-    // }
-    // console.log(props)
-
+    const { currentUser } = useAuth()
     const [selectedDate, setSelectedDate] = useState(null)
     const [tripNotes, setTripNotes] = useState(null)
+
     
     const handleSelectedDate =(date) =>{
         setSelectedDate(date)
@@ -58,16 +29,13 @@ const AddTrip = (props) => {
 
     const handleAddDB = () => {
         let strDate = JSON.stringify(selectedDate)
-        console.log(strDate)
         let year = strDate.slice(1, 5)
         let day = strDate.slice(9,11)
         let mnth = strDate.slice(6,8)
-        console.log('selected datesdfaawefas')
         const dateFinal = [mnth, day, year].join("-")
-        console.log('final input .........')
-        console.log(dateFinal)
-        console.log(tripNotes)
+        set(ref(db, `users/${currentUser.uid}/summits/${props.id}/trips`), {[dateFinal]:tripNotes})
     }
+
     return ( props.trigger) ? (
         <div className="popup">
             <div className="popup-inner">
