0c99bd37b7a8209095522e9f039cbf62e180bd97
Lia Gaetano <lgaetano19@gmail.com>
2022-02-04 14:52:10 -0800

    Weather API functionality now triggers new DB pull

--- Project/weathering-heights/src/components/App.js
+++ Project/weathering-heights/src/components/App.js
@@ -1,34 +1,29 @@
 import React from 'react';
-import axios from "axios";
-import { useState, useEffect, useContext } from 'react';
-import { ref, onValue, set, update } from "firebase/database";
+import { useState, useEffect, useRef } from 'react';
+import { ref, onValue } from "firebase/database";
 import { db } from './../firebase.js';
-import { Container } from 'react-bootstrap'
+import { Container } from 'react-bootstrap';
 import { AuthProvider } from '../contexts/AuthContext';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { getBulgerListCoords } from '../api/BulgerAPI'
-import { GetWeatherAPI } from '../api/GetWeatherAPI';
+import { getBulgerListCoords } from '../api/BulgerAPI';
 import Login from './Login';
 import Signup from './Signup';
 import PrivateRoute from './PrivateRoute';
 import ForgotPassword from './ForgotPassword';
 import Homepage from './Homepage';
 import MyProfile from './MyProfile';
+import UpdateWeatherButton from './UpdateWeatherButton.js';
 
 function App() {
   const [peakList, setPeakList] = useState([]);
   const [status, setStatus] = useState(true);
   const [coordinates, setCoordinates] = useState([]);
 
+  const [updateWeather, setUpdateWeather] = useState(0);
+
   // GET peak data from FBDB
   useEffect(() => {
     const bulgerListArr = [];
-
-    // In some cases it may be useful for a callback to be called once and then 
-    // immediately removed. We've created a helper function to make this easy:
-    // ref.once('value', (data) => {
-      // do some stuff once
-    // });
 
     const peaks = ref(db, 'peaks/');
     onValue(peaks, (snapshot) => {
@@ -51,65 +46,30 @@ function App() {
             };
         };
         setPeakList(bulgerListArr);
-
-      // });
     }, {onlyOnce: true});
-  }, []);
+  }, [updateWeather]);
 
-  // Extracts peak coordinates once peakList state has set
+  // Extracts peak coordinates to state once peakList state has set
   useEffect(() => {
     if (peakList !== []) {
       const peakCoords = getBulgerListCoords(peakList);
       setCoordinates(peakCoords);
     }
   }, [peakList]);
-    
 
-  // GET OpenWeatherMap weather data for each peak
-  useEffect(() => {
-    const weatherData = []
-
-    const baseURL = "http://api.weatherapi.com/v1"
-    // const api_key = process.env.REACT_APP_WEATHER_APP_API_KEY
-    const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
-
-    if (coordinates !== []) {
-      // Make API call for each peak coordinates
-      for (let i = 49; i < Object.keys(coordinates).length; i++) {
-        // Lat, Lon truncated to four decimals
-        let lat = parseFloat(coordinates[i].lat)
-        lat = lat.toFixed(4);
-        let lon = parseFloat(coordinates[i].lon)
-        lon = lon.toFixed(4);
-
-        let key = peakList[i].key;
-
-      // ***COMMENT OUT WHEN NOT IN USE***
-      // Weather API calls
-      axios
-          // Connects to Weather API at lat & lon
-          .get(`${baseURL}/current.json?key=${apiKey}&q=${lat},${lon}&aqi=no`)
-          .then((res) => {
-            const now = res.data.current;
-            // Updates temperature data in without override
-            // Will trigger onValue, which will update state
-            update(ref(db, 'peaks/' + key), {
-              temp: now.temp_f,
-              chance_precip: now.precip_in,
-              wind_speed: `${now.wind_mph} ${now.wind_dir}`
-            });
-          })
-          .catch((err) => {
-              console.log(err.data);
-          });
-        }
-      }
-      console.log(peakList)
-    }, [coordinates]);
-
+  // Toggles updateWeather state, which then triggers DB pull
+  const signalDBPull = () => {
+    setUpdateWeather(updateWeather + 1);
+  }
 
   return (
       <Container className="d-flex align-items-center" style={{ minHeight: "100vh" }}>
+        
+        <UpdateWeatherButton 
+          peakList={peakList}  
+          coordinates={coordinates}
+          signalDBPull={signalDBPull} />
+
         <div className="w-100" style={{ maxWidth: '400px'}}>
           <Router>
             <AuthProvider>
