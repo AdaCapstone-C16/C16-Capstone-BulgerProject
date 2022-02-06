9f0cfafe048f2e136f69d5742fc8b3c985eef34b
Roslyn Melookaran <rmelookaran@gmail.com>
2022-02-06 15:03:35 -0800

    The update button is now connected to the db

--- Project/weathering-heights/src/components/UpdateTrip.js
+++ Project/weathering-heights/src/components/UpdateTrip.js
@@ -9,19 +9,22 @@ import PropTypes from 'prop-types';
 
 
 const UpdateTrip = ({trigger, setTrigger, updateTrip, date}) => {
+    const [tripNotes, setTripNotes] = useState(null)
 
     // const { currentUser } = useAuth()
 
     // const handleDeleteSummit = () => {
     //     // set(ref(db, `users/${currentUser.uid}/summits/${props.id}`), {name:null})
     // }
-    const newDesc = 'NewDesc'
-
+    // const newDesc = 'NewDesc'
+    const handleTripNotes = (input) => {
+        setTripNotes(input.target.value)
+    }
     const handleYes = () => {
         // handleDeleteSummit()
         console.log("you chose yes")
         // props.updateList()
-        updateTrip(date, newDesc)
+        updateTrip(date, tripNotes)
         setTrigger(false) 
     }
 
@@ -36,8 +39,12 @@ const UpdateTrip = ({trigger, setTrigger, updateTrip, date}) => {
             <div className="popup-inner">
                 <h2>Would you like to update this trip?</h2>
                 {/* <button className="close-button" onClick={handleYes}>Add!</button> */}
-                <button  onClick={handleCancel}>No</button>
-                <button  onClick={handleYes}>Yes</button>
+                <form>
+                    <label>Update Trip {date}:</label>
+                    <textarea rows="5" cols="33" placeholder='Enter trip notes!' onChange={handleTripNotes}></textarea>
+                </form>
+                <button  onClick={handleCancel}>Cancel</button>
+                <button  onClick={handleYes}>Submit</button>
                 {/* {props.children} */}
             </div>
         </div>
