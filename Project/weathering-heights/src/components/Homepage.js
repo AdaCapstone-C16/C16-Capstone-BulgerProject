532c51f85d84e5edfa338ac8ea145e6e1f5b9683
Areeba Rashid <77311713+AreebaQ@users.noreply.github.com>
2022-02-06 11:24:23 -0800

    updated main with changes not reflected post PR

--- Project/weathering-heights/src/components/Homepage.js
+++ Project/weathering-heights/src/components/Homepage.js
@@ -1,12 +1,15 @@
 
 import React from 'react';
+//import { Accordion } from 'react-bootstrap';
 import {Link} from 'react-router-dom'
+import Accordion from './Accordion';
 
-export default function Homepage() {
+export default function Homepage(props) {
     return (
     <section>
         <Link to='/login'>Login</Link>
         <h1> Weathering Heights </h1>
+        <Accordion data={props.data}></Accordion>
     </section>   
     )
 }
